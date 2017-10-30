7e8465777826b3b26fd8d11d10cd0045821885cb
Martin Pokorný <MartinPokorny.czech@gmail.com>
2017-10-30 13:46:24 +0100

    oprava chyby %~dp0

--- res4dist/start__previewer.bat
+++ res4dist/start__previewer.bat
@@ -1,2 +1,2 @@
-start javaw -Djava.util.logging.config.file="%~dp0logging.properties" -cp "%~dp0zxs_udg_editor.jar" cz.mp.zxs.tools.udg_editor.UdgFilePreviewFrame "$script_dir/udg_data_examples.txt"
+start javaw -Djava.util.logging.config.file="%~dp0logging.properties" -cp "%~dp0zxs_udg_editor.jar" cz.mp.zxs.tools.udg_editor.UdgFilePreviewFrame %~dp0udg_data_examples.txt
 @sleep 1
